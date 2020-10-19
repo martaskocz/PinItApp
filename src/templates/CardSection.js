066e454ba9cb737379661a76c2d8cf9db6ada00f
Marta Skoczykloda <marta.sobstyl@gmail.com>
2020-10-19 22:59:19 +0200

    Create Input atom

--- src/templates/CardSection.js
+++ src/templates/CardSection.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SideBar from 'components/organisms/SideBar/SideBar';
-import styles from './CardSection.module.scss';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
+import Input from 'components/atoms/Input/Input';
 import Heading from '../components/atoms/Heading/Heading';
-import Paragraph from '../components/atoms/Paragraph/Paragraph';
+import styles from './CardSection.module.scss';
 
 const CardSection = ({ children, type }) => (
   <>
     <SideBar type={type} />
-    <Heading big name="Notes" />
-    <Paragraph additionalInfo name="12 notes" />
+    <div className={styles.cardSectionNav}>
+      <Input />
+      <Heading big name="Notes" />
+      <Paragraph additionalInfo name="12 notes" />
+    </div>
     <div className={styles.cardSection}>{children}</div>
   </>
 );
