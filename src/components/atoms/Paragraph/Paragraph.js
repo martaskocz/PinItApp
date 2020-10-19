497ffa460afdd51f4d303e9462612ac4e1055e77
Marta Skoczykloda <marta.sobstyl@gmail.com>
2020-10-19 22:23:57 +0200

    Adjust paragraph atom

--- src/components/atoms/Paragraph/Paragraph.js
+++ src/components/atoms/Paragraph/Paragraph.js
@@ -2,15 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Paragraph.module.scss';
 
-const Paragraph = ({ name }) => {
-  return <p className={styles.paragraph}>{name}</p>;
+const Paragraph = ({ name, additionalInfo }) => {
+  const className = additionalInfo ? styles.paragraphInfo : styles.paragraph;
+  return <p className={className}>{name}</p>;
 };
 
 Paragraph.propTypes = {
+  additionalInfo: PropTypes.bool,
   name: PropTypes.string,
 };
 
 Paragraph.defaultProps = {
+  additionalInfo: false,
   name: 'Lorem ipsum',
 };
 
--- src/templates/CardSection.js
+++ src/templates/CardSection.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import SideBar from 'components/organisms/SideBar/SideBar';
 import styles from './CardSection.module.scss';
 import Heading from '../components/atoms/Heading/Heading';
+import Paragraph from '../components/atoms/Paragraph/Paragraph';
 
 const CardSection = ({ children, type }) => (
   <>
     <SideBar type={type} />
     <Heading big name="Notes" />
+    <Paragraph additionalInfo name="12 notes" />
     <div className={styles.cardSection}>{children}</div>
   </>
 );
