71cb5354b092056146e6331a6e8e87bb8e5f647d
Marta Skoczykloda <marta.sobstyl@gmail.com>
2020-10-21 14:30:31 +0200

    Configure Card props

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -6,29 +6,27 @@ import ParagraphXS from 'components/atoms/ParagraphXS/ParagraphXS';
 import Button from 'components/atoms/Button/Button';
 import styles from './Card.module.scss';
 
-const Card = ({ type, name }) => {
+const Card = ({ articleUrl, content, created, type, title, twitterName }) => {
   const headingType = styles[type];
 
   return (
     <div className={styles.card}>
       <div className={`${styles.cardHeading} ${headingType}`}>
-        <Heading name={name} />
-        <ParagraphXS name="3 days" />
-        {type === 'twitter' && (
-          <img alt="twitter avatar" src="https://unavatar.now.sh/twitter/hello_roman" />
-        )}
+        <Heading title={title} />
+        <ParagraphXS name={created} />
+        {type === 'twitter' && <img alt="twitter avatar" src={twitterName} />}
         {type === 'article' && (
           <a
             v-text="icon"
             aria-label="article link"
             className={styles.iconLink}
-            href="https://google.com"
+            href={articleUrl}
           />
         )}
       </div>
       <div className={styles.cardBody}>
         <div className={styles.cardBodyPara}>
-          <Paragraph />
+          <Paragraph>{content}</Paragraph>
           <ParagraphXS bold name="READ MORE" />
         </div>
         <Button label="Remove" secondary />
@@ -39,12 +37,17 @@ const Card = ({ type, name }) => {
 
 Card.propTypes = {
   type: PropTypes.oneOf(['twitter', 'note', 'article']),
-  name: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  created: PropTypes.string.isRequired,
+  twitterName: PropTypes.string,
+  articleUrl: PropTypes.string,
+  content: PropTypes.string.isRequired,
 };
 
 Card.defaultProps = {
   type: 'twitter',
-  name: 'Heading',
+  twitterName: null,
+  articleUrl: null,
 };
 
 export default Card;
--- src/components/organisms/SideBar/SideBar.js
+++ src/components/organisms/SideBar/SideBar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ButtonLinkIcon from 'components/atoms/ButtonLinkIcon/ButtonLinkIcon';
+import Logo from 'components/atoms/Logo/Logo';
 import ArticleIcon from 'assets/icons/article.svg';
 import NoteIcon from 'assets/icons/note.svg';
 import TwitterIcon from 'assets/icons/twitter.svg';
 import LogoutIcon from 'assets/icons/logout.svg';
-import Logo from 'components/atoms/Logo/Logo';
 import styles from './SideBar.module.scss';
 
 const SideBar = ({ type }) => {
