25590ef243b4fc21a9e5b6016beadfdf93b7453c
Marta Skoczykloda <marta.sobstyl@gmail.com>
2020-10-27 14:10:05 +0100

    Move CardDetails to itself template

--- src/templates/CardDetails.js
+++ src/templates/CardDetails.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import Article from '../components/molecules/Article/Article';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
+import Heading from 'components/atoms/Heading/Heading';
+import Paragraph from 'components/atoms/Paragraph/Paragraph';
+import Button from 'components/atoms/Button/Button';
+import styles from './CardDetails.module.scss';
 
-const CardDetails = () => (
-  <div>
-    <Article title="My best note" dateInfo="CREATED 25-09-2020" content="Lorem ipsum" />
-  </div>
-);
+const CardDetails = ({ content, dateInfo, title }) => {
+  const history = useHistory();
+  const handleGoBack = () => {
+    history.push('/');
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <Heading big title={title} />
+      <Paragraph date content={dateInfo} />
+      <Paragraph content={content} />
+      <Button primary label="CLOSE / SAVE" onClick={handleGoBack} />
+      <Button asPlainText label="remove note" />
+    </div>
+  );
+};
+
+CardDetails.propTypes = {
+  content: PropTypes.string,
+  dateInfo: PropTypes.string,
+  title: PropTypes.string,
+};
+
+CardDetails.defaultProps = {
+  content: 'Content',
+  dateInfo: 'DD-MM-YYYY',
+  title: 'Title',
+};
 
 export default CardDetails;
--- src/views/Articles.js
+++ src/views/Articles.js
@@ -1,49 +1,7 @@
 import React from 'react';
 import CardSection from 'templates/CardSection';
 import Card from 'components/molecules/Card/Card';
-
-const articles = [
-  {
-    id: 1,
-    title: 'React on my mind',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    articleUrl: 'https://youtube.com',
-    created: '1 day',
-  },
-  {
-    id: 2,
-    title: 'Live React',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    articleUrl: 'https://youtube.com',
-    created: '3 days',
-  },
-  {
-    id: 3,
-    title: 'You do not know JS',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    articleUrl: 'https://youtube.com',
-    created: '12 days',
-  },
-  {
-    id: 4,
-    title: 'Cool vibes',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    articleUrl: 'https://youtube.com',
-    created: '13 days',
-  },
-  {
-    id: 5,
-    title: 'Winter is coming',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    articleUrl: 'https://youtube.com',
-    created: '15 days',
-  },
-];
+import { articles } from './articlesData';
 
 const Articles = () => (
   <CardSection type="article">
