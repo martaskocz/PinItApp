1ce9a9164b14ac4df17ddbda3fd93d319cd57aa2
Marta Skoczykloda <marta.sobstyl@gmail.com>
2020-10-27 13:34:47 +0100

    Add GoBack function

--- src/components/molecules/Article/Article.js
+++ src/components/molecules/Article/Article.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import styles from './Article.module.scss';
 import Heading from '../../atoms/Heading/Heading';
 import Paragraph from '../../atoms/Paragraph/Paragraph';
 import Button from '../../atoms/Button/Button';
 
 const Article = ({ content, dateInfo, title }) => {
+  const history = useHistory();
+  const handleGoBack = () => {
+    history.push('/');
+  };
+
   return (
     <div className={styles.wrapper}>
       <Heading big title={title} />
       <Paragraph date content={dateInfo} />
       <Paragraph content={content} />
-      <Button primary label="CLOSE / SAVE" />
+      <Button primary label="CLOSE / SAVE" onClick={handleGoBack} />
       <Button asPlainText label="remove note" />
     </div>
   );
