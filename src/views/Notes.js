e4e072ed320094e63a0ba72fbb02c66a3dbb36c3
Marta Skoczykloda <marta.sobstyl@gmail.com>
2020-10-29 09:46:26 +0100

    Refactor Notes

--- src/views/Notes.js
+++ src/views/Notes.js
@@ -4,15 +4,13 @@ import PropTypes from 'prop-types';
 import Card from 'components/molecules/Card/Card';
 import CardSection from 'templates/CardSection';
 
-const Notes = ({ notes }) => {
-  return (
-    <CardSection type="note">
-      {notes.map(({ content, created, id, title }) => (
-        <Card id={id} type="note" title={title} content={content} created={created} key={id} />
-      ))}
-    </CardSection>
-  );
-};
+const Notes = ({ notes }) => (
+  <CardSection type="note">
+    {notes.map(({ content, created, id, title }) => (
+      <Card id={id} type="note" title={title} content={content} created={created} key={id} />
+    ))}
+  </CardSection>
+);
 
 const mapStateToProps = (state) => {
   const { notes } = state;
