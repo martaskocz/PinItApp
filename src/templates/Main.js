b17cf2aba4e2373c89e5d375f078e015066f37df
Marta Skoczykloda <marta.sobstyl@gmail.com>
2020-11-05 10:11:15 +0100

    Add setCurrentPage method

--- src/templates/Main.js
+++ src/templates/Main.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
 import GlobalStyle from 'components/theme/GlobalStyle';
 
-const Main = ({ children }) => (
-  <div>
-    <GlobalStyle />
-    <>{children}</>
-  </div>
-);
+class Main extends React.Component {
+  state = {
+    pageType: 'notes',
+  };
 
-export default Main;
+  componentDidMount() {
+    this.setCurrentPage();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.setCurrentPage(prevState);
+  }
+
+  setCurrentPage = (prevState = '') => {
+    const pageTypes = ['twitters', 'articles', 'notes'];
+    const { pageType } = this.state;
+    const {
+      location: { pathname },
+    } = this.props;
+    const [currentPage] = pageTypes.filter((page) => pathname.includes(page));
+    if (prevState.pageType !== currentPage) {
+      this.setState({ pageType: currentPage });
+    }
+    return pageType;
+  };
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div>
+        <GlobalStyle />
+        <>{children}</>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Main);
 
 Main.propTypes = {
   children: PropTypes.element.isRequired,
+  location: PropTypes.shape().isRequired,
 };
