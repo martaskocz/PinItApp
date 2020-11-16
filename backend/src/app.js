1768b58a87fce11f2deed19592c744e377224cee
Marta Skoczykloda <marta.sobstyl@gmail.com>
2020-11-16 23:36:14 +0100

    Backend configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const routes = require('./routes');
 const User = require('./models/User');
 
-
+require('dotenv').config();
 const PORT = process.env.PORT || 9000;
 
 const app = express();
